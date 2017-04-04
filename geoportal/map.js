a786faa7d61585fb8c1beea1e4cdfb322a062e6f
Minh Pham <phamhongminh@live.com>
2017-04-04 00:22:34 -0600

    now can get data in rectangle

--- geoportal/map.js
+++ geoportal/map.js
@@ -148,7 +148,7 @@ var CartoDBLayer = function (n, u, c) {
     else
         table_name = 'zipcode';
 
-    var withinRect = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE public.{{table}}.the_geom @ ST_MakeEnvelope({{left}}, {{bottom}}, {{right}}, {{top}}, 4326)";
+    var withinRect = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE public.{{table}}.the_geom && ST_MakeEnvelope({{left}}, {{bottom}}, {{right}}, {{top}}, 4326)";
     var withinCircle = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE ST_Distance_Sphere(the_geom, ST_MakePoint({{lon}}, {{lat}})) <= {{radius}}";
 
     this.putOnMap = function () {
@@ -186,36 +186,13 @@ var CartoDBLayer = function (n, u, c) {
                         openInfoWindow(table_name, circle,c);
                     }
                 }
-
-                
-                for (var r = 0; r < rectangle.length; r++) {
-                    alert("EXECUTE");
-                    sql.execute(withinRect, { table: table_name, left: rectangle[r][0], bottom: rectangle[r][1], right: rectangle[r][2], top: rectangle[r][3] })
-                        .done(function (data) {
-                            alert(data.rows[0].b);
-                            var contentString = '<div class="infobox"><h3>AVERAGE DATA IN THAT REGION</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a
-                               + "</p><h4>PERCENT ABOUT ALCOHOL</h4><p>" + data.rows[0].b
-                               + "</p><h4>PERCENT ABOUT EXERCISE</h4><p>" + data.rows[0].c
-                               + "</p><h4>PERCENT ABOUT FAST FOOD</h4><p>" + data.rows[0].d
-                               + "</p><h4> PERCENT ABOUT FOOD</h4><p>" + data.rows[0].e
-                               + "</p><h4>PERCENT THAT ARE HAPPY</h4><p>" + data.rows[0].f
-                               + "</p><h4>PERCENT ABOUT HEALTHY FOOD</h4><p>" + data.rows[0].g
-                               + "</p><h4>PERCENT ABOUT ALCOHOL THAT ARE HAPPY</h4><p>" + data.rows[0].h
-                               + "</p><h4>PERCENT OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>" + data.rows[0].i
-                               + "</p><h4>PERCENT ABOUT FAST FOOD THAT ARE HAPPY</h4><p>" + data.rows[0].j
-                               + "</p><h4>PERCENT OF FOOD TWEETS THAT ARE HAPPY</h4><p>" + data.rows[0].k
-                               + "</p><h4>PERCENT ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>" + data.rows[0].l + "</p></div>";
-
-                            // Replace the info window's content and position.
-                            var infoWindow = new google.maps.InfoWindow();
-                            infoWindow.setContent(contentString);
-
-                            infoWindow.setPosition(google.maps.ControlPosition.TOP_CENTER);
-                            infoWindow.open(map);
-                        }).error(function (errors) {
-                            alert(errors[0]);
-                        });
+		if (rectangle.length > 0) {
+                    for (var r = 0; r < rectangle.length; r++) {
+                        openInfoWindowRectangle(table_name, rectangle, r);
+                    }
                 }
+                
+                
             });
 
         });
@@ -261,6 +238,43 @@ var CartoDBLayer = function (n, u, c) {
                 alert(errors[0]);
             });
     }
+ 
+     function openInfoWindowRectangle(table_name, rectangle, r) {
+        var ne = rectangle[r].getNorthEast();
+        var sw = rectangle[r].getSouthWest();
+
+        var infoWindow = new google.maps.InfoWindow({
+            position: sw,
+        });
+        var number = r + 1;
+        var contentString = '<div class="infobox"><h3>AVERAGE DATA IN RECTANGLE #' + number;
+
+        var sql = new cartodb.SQL({ user: 'hashtaghealth' });
+        
+        sql.execute(withinRect, { table: table_name, left: sw.lng(), bottom: sw.lat(), right: ne.lng(), top: ne.lat() })
+            .done(function (data) {
+                contentString += '</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT ALCOHOL</h4><p>' + data.rows[0].b.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT EXERCISE</h4><p>' + data.rows[0].c.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT FAST FOOD</h4><p>' + data.rows[0].d.toFixed(4)
+                   + '</p><h4> PERCENT ABOUT FOOD</h4><p>' + data.rows[0].e.toFixed(4)
+                   + '</p><h4>PERCENT THAT ARE HAPPY</h4><p>' + data.rows[0].f.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT HEALTHY FOOD</h4><p>' + data.rows[0].g.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT ALCOHOL THAT ARE HAPPY</h4><p>' + data.rows[0].h.toFixed(4)
+                   + '</p><h4>PERCENT OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].i.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT FAST FOOD THAT ARE HAPPY</h4><p>' + data.rows[0].j.toFixed(4)
+                   + '</p><h4>PERCENT OF FOOD TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].k.toFixed(4)
+                   + '</p><h4>PERCENT ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>' + data.rows[0].l.toFixed(4) + '</p></div>';
+
+                // Replace the info window's content and position.
+                infoWindow.setContent(contentString);
+                infoWindow.open(map);
+            }).error(function (errors) {
+                alert(errors[0]);
+            });
+    }	
+    	
+	
 };
 
 //layers.push(	new LayerContainer('Subdivisions', 'https://www.cartedesign.com/farmington/subdivisions2.kmz', 'City Layers'));
@@ -279,11 +293,7 @@ layers.push(	new CartoDBLayer('ZIP code', '', 'Map Layers'));
 
 function initialize() {
    geocoder = new google.maps.Geocoder();
-    //-----------------------------DRAWING MANAGER AND ITS CONTENT-----------------------------------------
-    //Creating a context menu to use it in event handler
-    DrawnMenuSetUp();
-    contextMenu = new ContextMenuDrawing(map);
-
+	
     drawingManager = new google.maps.drawing.DrawingManager(drawnOptions);
     drawingManager.setMap(map);
     drawingManager.setDrawingMode(null);
@@ -291,46 +301,16 @@ function initialize() {
     google.maps.event.addListener(drawingManager, 'circlecomplete', function (shape) {
         if (shape == null || (!(shape instanceof google.maps.Circle))) return;
         circle.push(shape);
-    });
-
-    google.maps.event.addListener(drawingManager, 'rectanglecomplete', function (shape) {
-        if (shape == null || (!(shape instanceof google.maps.RECTANGLE))) return;
-        var ne = shape.getBounds().getNorthEast();
-        var sw = shape.getBounds().getSouthWest();
-
-        var r = new Array();
-        r.push(sw.lng());
-        r.push(sw.lat());
-        r.push(ne.lng());
-        r.push(ne.lat());
-
-        rectangle.push(r);
-
-    });
+    });   
     google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event1) {
-
         polygonArray.push(event1.overlay);
-
         drawingManager.setDrawingMode(null);
-        google.maps.event.addListener(event1.overlay, 'rightclick', function (event) {
-            contextMenu.show(event.latLng);
-            document.getElementById('rm').addEventListener('click', function () {
-                event1.overlay.setMap(null);
-            });
-        });
+        if (event.type === google.maps.drawing.OverlayType.RECTANGLE) {
+            rectangle.push(event.overlay.getBounds());
+        }
     });
 
-    MapMenuSetUp();
-    mapMenu = new ContextMenuMap(map);
-    google.maps.event.addListener(map, 'rightclick', function (e) {
-        mapMenu.show(e.latLng);
-        document.getElementById('add').addEventListener('click', function () {
-            addToRegions(e.latLng);
-        });
-        document.getElementById('not add').addEventListener('click', function () {
-            removeFromRegions(e.latLng);
-        });
-    });
+    
   //geocoder = new google.maps.Geocoder();
   //directionsDisplay = new google.maps.DirectionsRenderer();
 
