f62e93fde205b590d27ef5d3663fd5a6f07136f6
Minh Pham <phamhongminh@live.com>
2017-04-05 22:38:15 -0600

    change method so map only shows data from 1 layer at a time

--- geoportal/map.js
+++ geoportal/map.js
@@ -6,6 +6,11 @@ var polygonArray = new Array();
 var circle = new Array();
 var rectangle = new Array();
 
+// these are PostGreSql. Functions such as ST_SetSRID, ST_Makepoint,etc. are from POSTGIS
+var withinRect = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE the_geom && ST_MakeEnvelope({{left}}, {{bottom}}, {{right}}, {{top}}, 4326)";
+var withinCircle = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE ST_DWITHIN(the_geom, ST_SetSRID(ST_MakePoint({{lon}}, {{lat}}),4326)::geography,{{radius}})";
+var withinPol = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE the_geom && ST_MakePolygon(LINESTRING(100 250, 100 350, 200 350, 200 250, 100 250))";
+
 var myOptions = {
     zoom: 4,
     center: latlng,
@@ -69,27 +74,14 @@ var layers = new Array();
 var cb_i = 0;
 
 //CartoDB Layer
-var CartoDBLayer = function (n,href, u, c) {
+var CartoDBLayer = function (table_name,href, u, c) {
     this.category = c;
-    this.name = n;
+    this.name = table_name;
     this.link = href;
     var l;
 
     cb_i++;
     var l_in = cb_i;
-
-    var table_name;
-    if (n == 'State')
-        table_name = 'states';
-    else if (n == 'County')
-        table_name = 'counties';
-    else
-        table_name = 'zipcode';
-
-    // these are PostGreSql. Functions such as ST_SetSRID, ST_Makepoint,etc. are from POSTGIS
-    var withinRect = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE the_geom && ST_MakeEnvelope({{left}}, {{bottom}}, {{right}}, {{top}}, 4326)";
-    var withinCircle = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE ST_DWITHIN(the_geom, ST_SetSRID(ST_MakePoint({{lon}}, {{lat}}),4326)::geography,{{radius}})";
-    var withinPol = "SELECT AVG(calories) as a, AVG(percentalc) as b, AVG(percentexe) as c,AVG(percentfas) as d,AVG(percentfoo) as e,AVG(percenthap) as f,AVG(percenthea) as g,AVG(sentalc) as h,AVG(sentex) as i,AVG(sentfastfo) as j,AVG(sentfood) as k,AVG(senthealth) as l FROM public.{{table}} WHERE the_geom && ST_MakePolygon(LINESTRING(100 250, 100 350, 200 350, 200 250, 100 250))";
 
     this.putOnMap = function () {
         cartodb.createLayer(map, u).addTo(map, l_in).on('done', function (layer) {
@@ -124,20 +116,6 @@ var CartoDBLayer = function (n,href, u, c) {
                     });
 
             });
-            $('#aggregate').on('click', function () {
-                if (circle.length > 0) {
-                    for (var c = 0; c < circle.length; c++) {
-                        // helper method was written below
-                        openInfoWindowCircle(table_name, circle, c);
-                    }
-                }
-                if (rectangle.length > 0) {
-                    for (var r = 0; r < rectangle.length; r++) {
-                        openInfoWindowRectangle(table_name, rectangle, r);
-                    }
-                }
-            });
-
         });
     };
     this.clearFromMap = function () {
@@ -150,74 +128,7 @@ var CartoDBLayer = function (n,href, u, c) {
         return false;
     };
 
-    //----------------HELPER METHOD TO OPEN INFOWINDOW FOR EACH SHAPE---------------------
-    //you can call infowindow of carto db, but I couldn't do it so I used infowindow of Google Maps 
-    function openInfoWindowCircle(table_name, circle, c) {
-
-        var infoWindow = new google.maps.InfoWindow({
-            position: circle[c].getCenter(),
-            pixelOffset: new google.maps.Size(-30, -30)
-        });
-        var number = c + 1;
-        var contentString = '<div class="infobox"><h3>AVERAGE DATA IN CIRCLE #' + number;
-        var sql = new cartodb.SQL({ user: 'hashtaghealth' });
-        sql.execute(withinCircle, { table: table_name, lon: circle[c].getCenter().lng(), lat: circle[c].getCenter().lat(), radius: circle[c].getRadius() })
-            .done(function (data) {
-
-                contentString += '</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT ALCOHOL</h4><p>' + data.rows[0].b.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT EXERCISE</h4><p>' + data.rows[0].c.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT FAST FOOD</h4><p>' + data.rows[0].d.toFixed(4)
-                    + '</p><h4> PROPORTION ABOUT FOOD</h4><p>' + data.rows[0].e.toFixed(4)
-                    + '</p><h4>PROPORTION THAT ARE HAPPY</h4><p>' + data.rows[0].f.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT HEALTHY FOOD</h4><p>' + data.rows[0].g.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT ALCOHOL THAT ARE HAPPY</h4><p>' + data.rows[0].h.toFixed(4)
-                    + '</p><h4>PROPORTION OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].i.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT FAST FOOD THAT ARE HAPPY</h4><p>' + data.rows[0].j.toFixed(4)
-                    + '</p><h4>PROPORTION OF FOOD TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].k.toFixed(4)
-                    + '</p><h4>PROPORTION ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>' + data.rows[0].l.toFixed(4) + '</p></div>';
-
-                infoWindow.setContent(contentString);
-                infoWindow.open(map);
-
-            }).error(function (errors) {
-                alert(errors[0]);
-            });
-    }
-    function openInfoWindowRectangle(table_name, rectangle, r) {
-        var ne = rectangle[r].getNorthEast();
-        var sw = rectangle[r].getSouthWest();
-
-        var infoWindow = new google.maps.InfoWindow({
-            position: ne,
-        });
-        var number = r + 1;
-        var contentString = '<div class="infobox"><h3>AVERAGE DATA IN RECTANGLE #' + number;
-
-        var sql = new cartodb.SQL({ user: 'hashtaghealth' });
-
-        sql.execute(withinRect, { table: table_name, left: sw.lng(), bottom: sw.lat(), right: ne.lng(), top: ne.lat() })
-            .done(function (data) {
-                contentString += '</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT ALCOHOL</h4><p>' + data.rows[0].b.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT EXERCISE</h4><p>' + data.rows[0].c.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT FAST FOOD</h4><p>' + data.rows[0].d.toFixed(4)
-                   + '</p><h4> PROPORTION ABOUT FOOD</h4><p>' + data.rows[0].e.toFixed(4)
-                   + '</p><h4>PROPORTION THAT ARE HAPPY</h4><p>' + data.rows[0].f.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT HEALTHY FOOD</h4><p>' + data.rows[0].g.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT ALCOHOL THAT ARE HAPPY</h4><p>' + data.rows[0].h.toFixed(4)
-                   + '</p><h4>PROPORTION OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].i.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT FAST FOOD THAT ARE HAPPY</h4><p>' + data.rows[0].j.toFixed(4)
-                   + '</p><h4>PROPORTION OF FOOD TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].k.toFixed(4)
-                   + '</p><h4>PROPORTION ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>' + data.rows[0].l.toFixed(4) + '</p></div>';
-
-                // Replace the info window's content and position.
-                infoWindow.setContent(contentString);
-                infoWindow.open(map);
-            }).error(function (errors) {
-                alert(errors[0]);
-            });
-    }
+ 
 
 
 };
@@ -228,8 +139,8 @@ var CartoDBLayer = function (n,href, u, c) {
 // The text of the button is set in itemtd2.innerHTML = "SOMETHING". 
 // in the CartoDBLayer, I called the text containing href : link
 // so I will set   itemtd2.innerHTML = layers[i].link;
-layers.push(new CartoDBLayer('State','State <a href="https://hashtaghealth.github.io/geoportal/state.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/state.xls" target="_blank">.xls)</a>', 'https://hashtaghealth.carto.com/api/v2/viz/a88b82ca-0900-11e7-b06b-0e3ff518bd15/viz.json', 'Map Layers'));
-layers.push(new CartoDBLayer('County','County <a href="https://hashtaghealth.github.io/geoportal/county.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/county.xls" target="_blank">.xls)</a>', 'https://hashtaghealth.carto.com/api/v2/viz/d61716ee-0e4d-11e7-9c2f-0ee66e2c9693/viz.json', 'Map Layers'));
+layers.push(new CartoDBLayer('states','State <a href="https://hashtaghealth.github.io/geoportal/state.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/state.xls" target="_blank">.xls)</a>', 'https://hashtaghealth.carto.com/api/v2/viz/a88b82ca-0900-11e7-b06b-0e3ff518bd15/viz.json', 'Map Layers'));
+layers.push(new CartoDBLayer('counties','County <a href="https://hashtaghealth.github.io/geoportal/county.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/county.xls" target="_blank">.xls)</a>', 'https://hashtaghealth.carto.com/api/v2/viz/d61716ee-0e4d-11e7-9c2f-0ee66e2c9693/viz.json', 'Map Layers'));
 layers.push(new CartoDBLayer('Census Tract','Census Tract <a href="https://hashtaghealth.github.io/geoportal/tract.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/tract.xlsx" target="_blank">.xlsx)</a>', '', 'Map Layers'));
 layers.push(new CartoDBLayer('ZIP code','Zip code <a href="https://hashtaghealth.github.io/geoportal/zipcode.txt" target="_blank">(.txt /</a><a href="https://hashtaghealth.github.io/geoportal/zipcode.xls" target="_blank">.xls)</a>', '', 'Map Layers'));
 
@@ -243,7 +154,7 @@ function initialize() {
 
     // Must set a layer on map. Else, you always need to click on first layer (State) 
     // before being able to choose on other layers
-    startVisible('County');
+    startVisible('counties');
     // Add a listener to show coordinate when right click
     google.maps.event.addListener(drawingManager, 'circlecomplete', function (shape) {
         if (shape == null || (!(shape instanceof google.maps.Circle))) return;
@@ -275,7 +186,95 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+//-------------------GET TABLE NAME------------------------------------
+function getTableName() {
+    var $radio = $('input[name=other]:checked');
+    var id = $radio.attr('id');
+    return id;
+}
+//-------------------HELPER METHODS TO AGGREGATE DATA----------------------------
+function getResults() {
+    var table_name = getTableName();
+    if (circle.length > 0) {
+        for (var c = 0; c < circle.length; c++) {
+            // helper method was written below
+            openInfoWindowCircle(table_name, circle, c);
+        }
+    }
+    if (rectangle.length > 0) {
+        for (var r = 0; r < rectangle.length; r++) {
+            openInfoWindowRectangle(table_name, rectangle, r);
+        }
+    }
+}
+//----------------HELPER METHOD TO OPEN INFOWINDOW FOR EACH SHAPE---------------------
+//you can call infowindow of carto db, but I couldn't do it so I used infowindow of Google Maps 
+function openInfoWindowCircle(table_name, circle, c) {
 
+    var infoWindow = new google.maps.InfoWindow({
+        position: circle[c].getCenter(),
+        pixelOffset: new google.maps.Size(-30, -30)
+    });
+    var number = c + 1;
+    var contentString = '<div class="infobox"><h3>AVERAGE DATA IN CIRCLE #' + number;
+    var sql = new cartodb.SQL({ user: 'hashtaghealth' });
+    sql.execute(withinCircle, { table: table_name, lon: circle[c].getCenter().lng(), lat: circle[c].getCenter().lat(), radius: circle[c].getRadius() })
+        .done(function (data) {
+
+            contentString += '</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT ALCOHOL</h4><p>' + data.rows[0].b.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT EXERCISE</h4><p>' + data.rows[0].c.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT FAST FOOD</h4><p>' + data.rows[0].d.toFixed(4)
+                + '</p><h4> PROPORTION ABOUT FOOD</h4><p>' + data.rows[0].e.toFixed(4)
+                + '</p><h4>PROPORTION THAT ARE HAPPY</h4><p>' + data.rows[0].f.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT HEALTHY FOOD</h4><p>' + data.rows[0].g.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT ALCOHOL THAT ARE HAPPY</h4><p>' + data.rows[0].h.toFixed(4)
+                + '</p><h4>PROPORTION OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].i.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT FAST FOOD THAT ARE HAPPY</h4><p>' + data.rows[0].j.toFixed(4)
+                + '</p><h4>PROPORTION OF FOOD TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].k.toFixed(4)
+                + '</p><h4>PROPORTION ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>' + data.rows[0].l.toFixed(4) + '</p></div>';
+
+            infoWindow.setContent(contentString);
+            infoWindow.open(map);
+
+        }).error(function (errors) {
+            alert(errors[0]);
+        });
+}
+function openInfoWindowRectangle(table_name, rectangle, r) {
+    var ne = rectangle[r].getNorthEast();
+    var sw = rectangle[r].getSouthWest();
+
+    var infoWindow = new google.maps.InfoWindow({
+        position: ne,
+    });
+    var number = r + 1;
+    var contentString = '<div class="infobox"><h3>AVERAGE DATA IN RECTANGLE #' + number;
+
+    var sql = new cartodb.SQL({ user: 'hashtaghealth' });
+
+    sql.execute(withinRect, { table: table_name, left: sw.lng(), bottom: sw.lat(), right: ne.lng(), top: ne.lat() })
+        .done(function (data) {
+            contentString += '</h3><br><h4>AVERAGE CALORIC DENSITY OF FOOD </h4><p>' + data.rows[0].a.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT ALCOHOL</h4><p>' + data.rows[0].b.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT EXERCISE</h4><p>' + data.rows[0].c.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT FAST FOOD</h4><p>' + data.rows[0].d.toFixed(4)
+               + '</p><h4> PROPORTION ABOUT FOOD</h4><p>' + data.rows[0].e.toFixed(4)
+               + '</p><h4>PROPORTION THAT ARE HAPPY</h4><p>' + data.rows[0].f.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT HEALTHY FOOD</h4><p>' + data.rows[0].g.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT ALCOHOL THAT ARE HAPPY</h4><p>' + data.rows[0].h.toFixed(4)
+               + '</p><h4>PROPORTION OF EXERCISE TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].i.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT FAST FOOD THAT ARE HAPPY</h4><p>' + data.rows[0].j.toFixed(4)
+               + '</p><h4>PROPORTION OF FOOD TWEETS THAT ARE HAPPY</h4><p>' + data.rows[0].k.toFixed(4)
+               + '</p><h4>PROPORTION ABOUT HEALTHY FOODS THAT ARE HAPPY</h4><p>' + data.rows[0].l.toFixed(4) + '</p></div>';
+
+            // Replace the info window's content and position.
+            infoWindow.setContent(contentString);
+            infoWindow.open(map);
+        }).error(function (errors) {
+            alert(errors[0]);
+        });
+}
 
 //-------------------HELPER METHODS FOR DRAWING MANAGER----------------
 function removeAll() {
